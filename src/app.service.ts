1d351abc135591daf3d011709c01fde5c50f0921
ilopez <ivan.lopez@igmweb.com>
2022-06-27 12:03:57 +0200

    feat:next1

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,27 @@ export class AppService {
   getHello(): string {
     return "Hello World!";
   }
+  public multiply(somenumber:number, otherNumber:number): number {
+    const multiply = somenumber * otherNumber;
+    return multiply;
+  }
+
+  public divide(somenumber:number, otherNumber:number): number {
+    if (otherNumber!==0){
+    const divide = somenumber / otherNumber;
+    return divide;
+    }else{
+      throw new Error(`${otherNumber} is no valid`);
+    }
+  }
+
+  public raiz(somenumber:number): number {
+    if (somenumber>=0){
+    const raiz = Math.sqrt(somenumber);
+    return raiz;
+    }else{
+      throw new Error(`${somenumber} is negative number`);
+    }
+  }
 }
+
